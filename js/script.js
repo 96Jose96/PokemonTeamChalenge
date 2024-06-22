b3d2b850b28982e1609db8ce46a81e0ffda089bb
Jose <jm.diaz6620@gmail.com>
2024-06-22 12:52:38 +0200

    solucionado problema comilla

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+
+
+
 const pokemonInfo = document.getElementById("pokemonInfo")
 const button = document.getElementById("get-pokemon")
 
@@ -6,7 +9,7 @@ const button = document.getElementById("get-pokemon")
 const getPokemon = (pokemon) => {
     fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
     .then((response) => {
-        if(!response.ok){
+        if(response !== data.ok){
             throw new Error("Error 404")
         }
         else{
@@ -15,15 +18,11 @@ const getPokemon = (pokemon) => {
     })
     .then(data => {
             let template = `<img src=${data.sprites.front_default} alt=${data.name} image>
-                            <h2><span>Name: </span>${data.name}</h2>
-                            <p><span>Height: </span>${data.height}</p>
-                            <p><span>Weight: </span>${data.weight}</p>
-                            <p><span>Type/s: </span> ${data.types.map(typeInfo => typeInfo.type.name).join(', ')}</p>`
+                            <h2><span>Nombre: </span>${data.name}</h2>
+                            <p><span>Altura: </span>${data.height}<p>
+                            <p><span>Peso: </span>${data.weight}</p>
+                            <p><span>Tipo/s: </span> ${data.types.map(typeInfo => typeInfo.type.name).join(', ')}</P>`
             pokemonInfo.innerHTML = template;
-            return data
-        })
-    .catch(() => {
-        console.error("Error 404")
     })
 }
 
@@ -32,5 +31,6 @@ button.addEventListener("click", () => {
     const select = document.getElementById("pokemon-select").value
     if(select !== ""){
         getPokemon(select)
+        
     }
 })
